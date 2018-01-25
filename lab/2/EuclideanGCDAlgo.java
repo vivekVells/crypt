c78855bfd22be065a248e984ffd4cad9a6ffdced
Vivek Vellaiyappan <techengineervivek@gmail.com>
2018-01-24 21:28:06 -0500

    Lab 2 initial commit. setting things up

---
+++ lab/2/EuclideanGCDAlgo.java
@@ -0,0 +1,37 @@
+/**
+* file: EuclideanGCDAlgo.java
+* author: Vivek Vellaiyappan Surulimuthu
+* course: MSCS630S
+* assignment: Lab 2 - part 1
+* due date: Wednesday, February 7, 2018.
+* 
+* This file contains the code to implement Euclidean algorithm in order to find GCD of given numbers.
+*/
+
+/*
+*	StringToInteger
+*	
+* This class implements Euclidean algorithm to find GCD of given numbers
+* 
+* Objective: GCD of given numbers using Euclidean algorithm
+*
+* Output: Please view at the end of the file. (You can also verify the output at "OutputEuclideanGCDAlgo.txt" file)
+*/
+
+// importing required java util packages
+// Scanner pkg to use Standard Sys input 
+import java.util.Scanner;
+
+public class EuclideanGCDAlgo {
+
+}
+
+/*
+* Program Objective:
+*  
+* Input:
+*
+* Output:
+* 
+*
+*/
