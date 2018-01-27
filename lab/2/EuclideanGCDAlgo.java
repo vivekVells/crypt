6052fb8be0d9e7dbe1f6d30143d0151fa89f5e61
Vivek Vellaiyappan <techengineervivek@gmail.com>
2018-01-26 23:26:21 -0500

    changed stuffs to make sure the implementation happening with or without recursion.

--- lab/2/EuclideanGCDAlgo.java
+++ lab/2/EuclideanGCDAlgo.java
@@ -27,38 +27,65 @@ public class EuclideanGCDAlgo {
   /*
   * getQuotientRemainder
   * 
-  * This function returns quotient and then remainder for any given integer numbers
+  * This function returns quotient and then remainder for any given long numbers
   * 
-  * Parameters: 2 integer numbers: a, b
+  * Parameters: 2 long numbers: a, b
   *
   * Return: returns quotient and remainder
   */
-  public static int[] getQuotientRemainder(int a, int b){
-    int[] tempQuotientRemainder = new int[2];
+  public static long[] getQuotientRemainder(long a, long b){
+    long[] tempQuotientRemainder = new long[2];
     tempQuotientRemainder[0] = a/b;
     tempQuotientRemainder[1] = a%b;
     return tempQuotientRemainder;
   }
-	
+
   /*
-  * getBiggerDividend
-  * 
-  * This function returns biggerDividendFirst the logic is made such that 'a' is divided by 'b'
-  *   so, to make that possible whenever 'a' is not greater than or equal to 'b', we have to make the swap
-  * 
-  * Parameters: 2 integer numbers: a, b
-  *
-  * Return: returns value such that first arg is always greater than the second arg
-  */	
-  public static int[] getBiggerDividend(int a, int b){
-    int[] tempDividendDivisor = new int[2];
-    if(!(a>=b)){
-    int temp = a;
-    a = b;
-    b = temp;
-    }
-    return tempDividendDivisor;
-  }
+	*euclicdAlgWithoutRecursion 
+	* 
+	* This function returns the GCD calculation of given long args using euclicdAlgorithm without recursion method
+	* 
+	* Parameters: long arg a, long arg b
+	* 
+	* Return: returns gcd of any given positive long numbers 
+	*/
+	public static long euclicdAlgWithoutRecursion(long a, long b){
+    long[] quotientRemainder = new long[2];
+    // this loop will execute until the remainder equals 0
+    do{
+      quotientRemainder = getQuotientRemainder(a,b);
+      System.out.println("Quotient & remainder: " + quotientRemainder[0] + "\t " + quotientRemainder[1]);
+      
+			// this will take care of a < b situation and if that happens, the swap among a & b value done such that a >= b
+      if(quotientRemainder[1] != 0){
+        a = b;
+        b = quotientRemainder[1];
+			}
+		}while(quotientRemainder[1] != 0);
+		return b;
+	}
+
+  /*
+	*euclicdAlgWithRecursion 
+	* 
+	* This function returns the GCD calculation of given long args using euclicdAlgorithm with recursion method
+	* 
+	* Parameters: long arg a, long arg b
+	* 
+	* Return: returns gcd of any given positive long numbers 
+	*/	
+	public static long euclicdAlgWithRecursion(long a, long b) {
+		long[] quotientRemainder = new long[2];
+		quotientRemainder = getQuotientRemainder(a,b);
+		
+		if(quotientRemainder[1] != 0){
+			a = b;
+			b = quotientRemainder[1];
+			return euclicdAlgWithRecursion(a,b);
+		}else{
+			return b;
+		}
+	}
 	
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
@@ -66,30 +93,25 @@ public class EuclideanGCDAlgo {
     System.out.println("\nGCD calculation using Euclidean Algorithm");
 
     while(scan.hasNextLine()){
-      int a = scan.nextInt(), b = scan.nextInt();
+      long a = scan.nextInt(), b = scan.nextInt();
 
       // I am sure there is some better way to retain the initial value of a and b. 
       //   Doing like this for now.
-      int aValueRetained = a, bValueRetained = b;
-      int[] quotientRemainder = new int[2];
-
+      long aValueRetained = a, bValueRetained = b;
       System.out.println("\nCalculation for GCD(" + aValueRetained + "," + bValueRetained +"): ");
-
-      // this loop will execute until the remainder equals 0
-      do{
-        getBiggerDividend(a,b);
-        quotientRemainder = getQuotientRemainder(a,b);
-        System.out.println("Quotient & remainder: " + quotientRemainder[0] + "\t " + quotientRemainder[1]);
-
-        if(quotientRemainder[1] != 0){
-        a = b;
-        b = quotientRemainder[1];
-        }
-     }while(quotientRemainder[1] != 0);
-
-     System.out.println("GCD(" + aValueRetained + "," + bValueRetained + ") = " + b);
-     System.out.println("-----------------------------------------------------\n");
+      
+			// to solve the program with and without recursion, the long euclicdAlg(long a, long b) function as per the program instruction was // being changed to euclicdAlgWithoutRecursion(long a, long b) and euclicdAlgWithRecursion(long a, long b)
+			
+			// without recursion
+			System.out.println("\nWithout Recursion: GCD(" + aValueRetained + "," + bValueRetained + ") = " + euclicdAlgWithoutRecursion(a,b));
+			
+			// with recursion
+			System.out.println("With Recursion   : GCD(" + aValueRetained + "," + bValueRetained + ") = " + euclicdAlgWithRecursion(a,b));
+			
+      System.out.println("-----------------------------------------------------\n");
+      scan.nextLine();
    }
+	 scan.close();
 	}
 	
 }
@@ -105,43 +127,68 @@ public class EuclideanGCDAlgo {
 * 148 75
 *
 * Output:
-* maristuser@LBESCRIP-35 MINGW64 ~/Desktop/MSCS630Lab2
+* Vivek-Pc@kev MINGW64 /e/Marist/Semester2/Cryptography/lab/2 (master)
 * $ javac EuclideanGCDAlgo.java
-*
-* maristuser@LBESCRIP-35 MINGW64 ~/Desktop/MSCS630Lab2
+
+* Vivek-Pc@kev MINGW64 /e/Marist/Semester2/Cryptography/lab/2 (master)
 * $ java EuclideanGCDAlgo < InputEuclideanGCDAlgo.txt
-* 
+
 * GCD calculation using Euclidean Algorithm
-* 
+
+* Calculation for GCD(75,148):
+* Quotient & remainder: 0  75
+* Quotient & remainder: 1  73
+* Quotient & remainder: 1  2
+* Quotient & remainder: 36         1
+* Quotient & remainder: 2  0
+
+* Without Recursion: GCD(75,148) = 1
+* With Recursion   : GCD(75,148) = 1
+* -----------------------------------------------------
+
+
+* Calculation for GCD(75,75):
+* Quotient & remainder: 1  0
+
+* Without Recursion: GCD(75,75) = 75
+* With Recursion   : GCD(75,75) = 75
+* -----------------------------------------------------
+
+
 * Calculation for GCD(100,74):
 * Quotient & remainder: 1  26
-*	Quotient & remainder: 2  22
-*	Quotient & remainder: 1  4
-*	Quotient & remainder: 5  2
-*	Quotient & remainder: 2  0
-*	GCD(100,74) = 2
-*	-----------------------------------------------------
-*
-*
+* Quotient & remainder: 2  22
+* Quotient & remainder: 1  4
+* Quotient & remainder: 5  2
+* Quotient & remainder: 2  0
+
+* Without Recursion: GCD(100,74) = 2
+* With Recursion   : GCD(100,74) = 2
+* -----------------------------------------------------
+
+
 *	Calculation for GCD(1071,462):
-*	Quotient & remainder: 2  147
-*	Quotient & remainder: 3  21
-*	Quotient & remainder: 7  0
-*	GCD(1071,462) = 21
-*	-----------------------------------------------------
+* Quotient & remainder: 2  147
+* Quotient & remainder: 3  21
+* Quotient & remainder: 7  0
+
+* Without Recursion: GCD(1071,462) = 21
+* With Recursion   : GCD(1071,462) = 21
+* -----------------------------------------------------
+
+
+* Calculation for GCD(148,75):
+* Quotient & remainder: 1  73
+* Quotient & remainder: 1  2
+* Quotient & remainder: 36         1
+* Quotient & remainder: 2  0
+
+* Without Recursion: GCD(148,75) = 1
+* With Recursion   : GCD(148,75) = 1
+* -----------------------------------------------------
 *
 *
-*	Calculation for GCD(148,75):
-*	Quotient & remainder: 1  73
-*	Quotient & remainder: 1  2
-*	Quotient & remainder: 36         1
-*	Quotient & remainder: 2  0
-*	GCD(148,75) = 1
-*-----------------------------------------------------
-*
-* 
-* maristuser@LBESCRIP-35 MINGW64 ~/Desktop/MSCS630Lab2
+* Vivek-Pc@kev MINGW64 /e/Marist/Semester2/Cryptography/lab/2 (master)
 * $
-*
-*
 */
+
