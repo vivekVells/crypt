0255ac0c0b39d0a6b51c95053031f24402d0fa55
Vivek Vellaiyappan <techengineervivek@gmail.com>
2018-01-27 14:03:18 -0500

    Update StringToInteger.java

--- lab/1/StringToInteger.java
+++ lab/1/StringToInteger.java
@@ -26,11 +26,11 @@ import java.util.Arrays;
 
 public class StringToInteger {
   // Static Global Constant Variable TOKEN_65 used 
-  //  to reduce from the converted alphabet ascii value to obtain 
-  //  objective of this program.
+  //   to reduce from the converted alphabet ascii value to obtain 
+  //   objective of this program.
   // for e.g.: "CAR" is converted into { 2 0 17 } 
-  //  (i.e.) (ASCII)"CAR"	- TOKEN_65^(times_the_alphabets_length_individually) 
-  //  => 67-65 65-65 82-65 => 2 0 17 
+  //   (i.e.) (ASCII)"CAR" - TOKEN_65^(times_the_alphabets_length_individually) 
+  //   => 67-65 65-65 82-65 => 2 0 17 
   static final int TOKEN_65 = 65;
 
   public static void main(String[] args) {
@@ -72,7 +72,6 @@ public class StringToInteger {
     }	
     return intStr;
   }
-
 }
 
 /*
